333281a3550b1c7fb8c5b8d962984491b6ab44fb
liangxue3956 <70993523+liangxue3956@users.noreply.github.com>
2020-10-13 23:16:17 +0800

    12

--- src/main/Sort.java
+++ src/main/Sort.java
@@ -1,20 +1,28 @@
 package main;
 
 import java.util.ArrayList;
-
+import java.util.Arrays;
+ 
 public class Sort {
-	public ArrayList Sort1(ArrayList arr){
-		int len= arr.size();
-		for(int i=0;i<len;i++){
-			for(int j=0;j<len-i-1;j++){//注意第二重循环的条件
-				//冒泡法排序，伪代码
-				if(arr[j]>arr[j+1]){
-					int temp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
-                	}
-    		}        
-       }
-		return arr;
+	public Sort(int[] numbers) {
+		for(int i=0;i<numbers.length-1;i++)
+        {
+            for(int j=0;j<numbers.length-1-i;j++)
+            {
+                if(numbers[j]>numbers[j+1])
+                {
+                    int temp=numbers[j];
+                    numbers[j]=numbers[j+1];
+                    numbers[j+1]=temp;
+                }
+            }
+        } 
+		for(int i=0;i<numbers.length;i++)
+            System.out.print(numbers[i]+" ");
+	}
+	public void xue(){}
+
+ 
 }
-}
+/*if((int)arr.get(j)>(int)arr.get(j+1)){
+	int temp=(int)arr.get(j);*/
