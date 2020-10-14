bddeb57982a762c17ed62d96ed3bc62ea7448c12
lalala3086 <70993416+lalala3086@users.noreply.github.com>
2020-10-14 10:55:54 +0800

    zxx

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,37 @@
 package main;
 import java.util.ArrayList;
+import java.util.List;
 
 import main.Sort;
 public class Main {//成绩统计
 	@SuppressWarnings("null")
 	public static void main (String [] args){
+		/************************成绩及格率********************
+		ArrayList<String> arr=new ArrayList<String>();
+		arr.add("50");
+		arr.add("80");
+		arr.add("60");
+		arr.add("20");
+		arr.add("80");
+		List<String>arrzxxList=new ArrayList<String>();
+		double rate=0.00;		
+		passrate  pass = new passrate();
+		rate=pass.passrate01(arr);
+		System.out.println("成绩及格率:"+rate+"%");
+		*****************************************************/		
 		ArrayList arr=new ArrayList();
 		arr.add(50);
 		arr.add(80);
 		arr.add(60);
 		arr.add(20);
 		arr.add(80);
-
-			
+		//成绩及格率
+		double passrate = (double) Passrate.passrate(arr);
+		System.out.println("及格率："+passrate+"%");
+		//平均分
 		double average = Average.average(arr);
 		System.out.println("平均分为:"+average);
-		
-		
+		//冒泡排序
 		Object[]  strArr =  arr.toArray();//转化为数组
 		int[] ll = new int[]{50,80,60,20,80};//创建int数组
 		for (int i=0;i<strArr.length;i++){//把数组中下标的数换位Arraylist中的数
@@ -26,9 +41,14 @@ public class Main {//成绩统计
 		 }
 		Sort s =new Sort(ll);//调用方法进行排序输出
 		s.xue();
+		//成绩最大值
+		//String lage;
+		//zhuizhi chen = new zhuizhi();
+		//lage=chen.zuizhi(arr);		
+		//System.out.println("最高成绩为:"+lage);
+
 
 	}
-	
-	
+
 
 }
--- src/main/Passrate.java
+++ src/main/Passrate.java
@@ -3,8 +3,28 @@ package main;
 import java.util.ArrayList;
 import java.util.List;
 
-public class passrate {
-	public double passrate01(List<String>arrzxxList) {
+public class Passrate {
+	public static double passrate (ArrayList arr) {
+		int n =arr.size();
+		int i =0;
+		double k=0;
+		for(i=0;i<n;i++) {
+			int d1=(int) arr.get(i);
+			
+			if(d1>60||d1==60) {
+				k=k+1;
+				
+			}
+			
+		}
+		double passrate = k/n*100;
+		
+		return passrate;
+		
+		
+	}
+}
+	/*public double passrate01(List<String>arrzxxList) {
 		List<String>arr1=new ArrayList<String>();
 		
 		for(int i=0;i<arrzxxList.size();i++) {
@@ -19,6 +39,7 @@ public class passrate {
 	return	(Double.valueOf(arr1.size())/arrzxxList.size())*100;
 		
 	
-	}
+	}*/
+	
 
-}
+
